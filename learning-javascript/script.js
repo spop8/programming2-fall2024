160698ca6e360cd5537ed14cc92dbabcc1e91dc2
Solena <sornelaspagnucci26@fwparker.org>
2024-11-18 22:02:47 -0600

    flexbox matching game

---
+++ learning-javascript/script.js
@@ -0,0 +1,12 @@
+const myName = "Solena";
+let age = 16;
+
+//const is a constant and the value can't be changed
+//let has a volue that can be changed
+//camel case = camelCase or myNameIsSolena
+//console is like print in python
+
+console.log(myName);
+console.log(age);
+console.log("Hello my name is " + myName + " and I am " + age);
+console.log(age + 5);
