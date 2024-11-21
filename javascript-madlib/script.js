003812015599396df7ff745f4e1aa661f1a36642
Solena <sornelaspagnucci26@fwparker.org>
2024-11-20 23:14:31 -0600

    javascript madlib

---
+++ javascript-madlib/script.js
@@ -0,0 +1,7 @@
+function generate() {
+	const a1 = document.getElementById("a1input").value;
+	const n1 = document.getElementById("n1input").value;
+
+	document.getElementById("a1").innerText = a1;
+	document.getElementById("n1").innerText = n1;
+}
--- learning-javascript/script.js
+++ learning-javascript/script.js
@@ -1,12 +1,23 @@
-const myName = "Solena";
-let age = 16;
+//const myName = "Solena";
+//let age = 16;
+//
+////const is a constant and the value can't be changed
+////let has a volue that can be changed
+////camel case = camelCase or myNameIsSolena
+////console is like print in python
+//
+//console.log(myName);
+//console.log(age);
+//console.log("Hello my name is " + myName + " and I am " + age);
+//console.log("The sume of 16+16 is " + age + age);
+//console.log(age + age + " is the sum of 16 + 16");
+//console.log("The sum of 16 + 16 is " + (age + age));
+//
+////^ called type coercion
+////instead of int or float to convert we use number() for ints and deci
 
-//const is a constant and the value can't be changed
-//let has a volue that can be changed
-//camel case = camelCase or myNameIsSolena
-//console is like print in python
-
-console.log(myName);
-console.log(age);
-console.log("Hello my name is " + myName + " and I am " + age);
-console.log(age + 5);
+function reveal() {
+	const mySpan = document.getElementById("name");
+	mySpan.innerText = "Solena"
+	//alert("This works!");
+}
