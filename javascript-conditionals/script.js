d094d6a66cf9817b291b79357e41124b8e15063e
Solena <sornelaspagnucci26@fwparker.org>
2024-11-25 13:49:50 -0600

    conditionals

--- javascript-conditionals/script.js
+++ javascript-conditionals/script.js
@@ -1,23 +1,38 @@
-//let age = 17;
+////let age = 17;
+////
+////if (age >= 18) {
+////	alert("You are an adult!!");
+////}
+////else if (age >= 13) {
+////	alert("You are a teenager");
+////}
+////else{
+////	alert("You are a child");
+////}
 //
-//if (age >= 18) {
-//	alert("You are an adult!!");
+//let score = 87;
+//
+//if (score == 90) {
+//	alert("You got an A!! 🥳");
+//} else if (score >= 80) {
+//	alert("You got an B 🙂");
+//} else if (score >= 70) {
+//	alert("You got an C 😢 ");
+//} else {
+//	alert("You need to study more 📚");
 //}
-//else if (age >= 13) {
-//	alert("You are a teenager");
-//}
-//else{
-//	alert("You are a child");
-//}
+// and = && or = ||
+let temp = 28;
+let condition = "sunny";
 
-let score = 90;
-
-if (score === 90) {
-	alert("You got an A!! 🥳");
-} else if (score >= 80) {
-	alert("You got an B 🙂");
-} else if (score >= 70) {
-	alert("You got an C 😢 ");
+if (temp >= 70 && condition == "sunny") {
+	alert("You should wear a tshirt");
+} else if (temp > 40 && (condition == "rainy" || condition == "stormy")) {
+	alert("You should wear a raincoat");
+} else if (temp <= 30 && condition == "snowy") {
+	alert("You should wear a winter coat");
+} else if (temp > 30 && temp < 50) {
+	alert("You should wear a coat");
 } else {
-	alert("You need to study more 📚");
+	alert("You should wear a hoodie");
 }
