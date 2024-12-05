2f7ba0c27b2f288f020edbc900f8d079d5ed1c93
Solena <sornelaspagnucci26@fwparker.org>
2024-12-05 15:40:38 -0600

    loops

---
+++ javascript-loops/script.js
@@ -0,0 +1,36 @@
+/*let fastFood = ["hamburger", "fries", "milkshake", "chicken nuggets"];
+
+for (let i = 0; i < fastFood.length; i++) {
+	console.log(fastFood[i]);
+}
+
+let numbers = [45, 23, 78];
+let doubled = [];
+
+for (let i = 0; i < numbers.length; i++) {
+	doubled.push(numbers[i] * 2);
+}
+console.log(doubled);
+
+for (let i = 0; i < 100; i++) {
+	console.log("Solena");
+}
+*/
+
+let animals = ["dog", "cat", "panda", "camel", "koala"];
+
+for (let i = 0; i < animals.length; i++) {
+	console.log(animals[i]);
+	let animals_upper = animals[i].toUpperCase();
+	console.log(animals_upper);
+}
+animals.pop();
+animals.pop();
+
+let score = [80, 75, 23, 64, 90, 100, 45, 89, 67, 93];
+for (let i = 0; i < score.length; i++) {
+	let new_score = score[i] + 5;
+	if (score[i] < 50) {
+		score.splice(i, 1, "retake");
+	}
+}
