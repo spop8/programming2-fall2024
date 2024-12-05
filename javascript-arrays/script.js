8894084bdb63afe717d2351070cada905c2c2825
Solena <sornelaspagnucci26@fwparker.org>
2024-12-04 23:42:22 -0600

    arrays

--- javascript-arrays/script.js
+++ javascript-arrays/script.js
@@ -20,9 +20,28 @@ alert(myList);
 
 // splice removes all items after number of item
 // slice removes all items before number of item
-*/
+
 let groceries = ["milk", "fruit", "bread", "eggs"];
 groceries.splice(1, 1, "ice cream");
 alert(groceries);
 groceries.splice(0, 1);
 alert(groceries);
+*/
+
+let items = ["phone", "computer", "book"];
+alert(items);
+items.push("pencil", "highlighter"); // add two items at end
+alert(items);
+items.pop(); // remove last item
+alert(items);
+items.splice(1, 0, "glasses"); // add new item to second place
+alert(items);
+items.splice(2, 1); // remove third item
+alert(items);
+
+let numbers = ["1", "2", "3"];
+alert(numbers);
+numbers.splice(0, 0, "4"); // add number at start
+alert(numbers);
+numbers.splice(1, 1, "6"); // replace second number
+alert(numbers);
