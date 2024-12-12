54d2da349fed16035b7740ef1eb67b7269a4c541
Solena <sornelaspagnucci26@fwparker.org>
2024-12-12 15:29:57 -0600

    new

---
+++ flexbox-matching-game/script.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", () => {
+	const pairsComida = [
+		{ word: "el menú", match: "menu" },
+		{ word: "el almuerzo", match: "lunch" },
+		{ word: "el/la camarero/a", match: "waiter" },
+		{ word: "el desayuno", match: "breakfast" },
+		{ word: "la sección de (no) fumar", match: "(no) smoking section" },
+		{ word: "la cena", match: "dinner" },
+		{ word: "los entremeses", match: "appetizers" },
+		{ word: "la cuenta", match: "check" },
+		{ word: "el/la dueño/a", match: "owner" },
+		{ word: "la propina", match: "tip" }
+	];
+	const pairsFrutas = [
+		{ word: "el menú", match: "menu" },
+		{ word: "el almuerzo", match: "lunch" },
+		{ word: "el/la camarero/a", match: "waiter" },
+		{ word: "el desayuno", match: "breakfast" },
+		{ word: "la sección de (no) fumar", match: "(no) smoking section" },
+		{ word: "la cena", match: "dinner" },
+		{ word: "los entremeses", match: "appetizers" },
+		{ word: "la cuenta", match: "check" },
+		{ word: "el/la dueño/a", match: "owner" },
+		{ word: "la propina", match: "tip" }
+	];
+	const pairsCarne = [
+		{ word: "el menú", match: "menu" },
+		{ word: "el almuerzo", match: "lunch" },
+		{ word: "el/la camarero/a", match: "waiter" },
+		{ word: "el desayuno", match: "breakfast" },
+		{ word: "la sección de (no) fumar", match: "(no) smoking section" },
+		{ word: "la cena", match: "dinner" },
+		{ word: "los entremeses", match: "appetizers" },
+		{ word: "la cuenta", match: "check" },
+		{ word: "el/la dueño/a", match: "owner" },
+		{ word: "la propina", match: "tip" }
+	];
+});
