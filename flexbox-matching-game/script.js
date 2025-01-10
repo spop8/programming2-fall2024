929cec4bcd1519f33be27cf7502fc7fd34d85a92
Solena <sornelaspagnucci26@fwparker.org>
2025-01-09 18:54:41 -0600

    matching game

--- flexbox-matching-game/script.js
+++ flexbox-matching-game/script.js
@@ -1,100 +1,26 @@
-let food = document.getElementById("category");
-if (food.value == "index.html") {
-	document.addEventListener("DOMContentLoaded", () => {
-		const pairs = [
-			{ word: "el menú", match: "menu" },
-			{ word: "el almuerzo", match: "lunch" },
-			{ word: "el/la camarero/a", match: "waiter" },
-			{ word: "el desayuno", match: "breakfast" },
-			{ word: "la sección de (no) fumar", match: "(non) smoking section" },
-			{ word: "la cena", match: "dinner" },
-			{ word: "los entremeses", match: "appetizers" },
-			{ word: "la cuenta", match: "check" },
-			{ word: "el/la dueño/a", match: "owner" },
-			{ word: "la propina", match: "tip" },
-			{ word: "la comida", match: "food" },
-			{ word: "el plato principal", match: "main dish" },
-			{ word: "delicioso/a ", match: "delicious" },
-			{ word: "rico/a ", match: "tasty" },
-			{ word: "sabroso/a ", match: "tasty" }
+let pairs = {};
+document.addEventListener("DOMContentLoaded", () => {
+	let food = document.getElementById("category");
+	if (food.value == "index.html") {
+		pairs = [
+			{ word: "el menú", match: "menu.jpg" },
+			{ word: "el almuerzo", match: "lunch.avif" },
+			{ word: "el/la camarero/a", match: "waiter.jpg" },
+			{ word: "el desayuno", match: "breakfast.jpg" },
+			{ word: "la sección de (no) fumar", match: "smoke.jpg" },
+			{ word: "la cena", match: "dinner.jpg" },
+			{ word: "los entremeses", match: "appetizer.jpg" },
+			{ word: "la cuenta", match: "bill.jpg" },
+			{ word: "el/la dueño/a", match: "owner.png" },
+			{ word: "la propina", match: "tips.png" },
+			{ word: "la comida", match: "food.jpg" }
+			//{ word: "el plato principal", match: "main dish" },
+			//{ word: "delicioso/a ", match: "delicious" },
+			//{ word: "rico/a ", match: "tasty" },
+			//{ word: "sabroso/a ", match: "tasty" }
 		];
-
-		let cards = document.getElementsByClassName("cards");
-		console.log(cards);
-
-		function shuffleArray(array) {
-			for (let i = array.length - 1; i > 0; i--) {
-				// Generate a random index from 0 to i
-				const j = Math.floor(Math.random() * (i + 1));
-				// Swap elements at indices i and j
-				[array[i], array[j]] = [array[j], array[i]];
-			}
-			return array;
-		}
-
-		function shuffleAssign() {
-			// shuffle list of pairs
-			let shuffledPairs = shuffleArray(pairs);
-			console.log(shuffledPairs);
-
-			//create a new array for selected pairs this round
-			let cardPairs = [];
-
-			//add the first cards.length/2 pairs with word and match to new array
-			for (let i = 0; i < cards.length / 2; i++) {
-				cardPairs.push(shuffledPairs[i].word);
-				cardPairs.push(shuffledPairs[i].match);
-			}
-			console.log(cardPairs);
-
-			//shuffle that array
-			cardPairs = shuffleArray(cardPairs);
-
-			// iterate over that array to assign cards
-			for (let i = 0; i < cardPairs.length; i++) {
-				cards[i].innerText = cardPairs[i];
-				cards[i].onclick = isClicked;
-			}
-
-			function isClicked(e) {
-				let cardClicked = e.srcElement;
-				cardClicked.classList.add("clicked");
-				let clicked = document.getElementsByClassName("clicked");
-				if (clicked.length == 2) {
-					if (isMatch(clicked[0].innerText, clicked[1].innerText)) {
-						clicked[0].style.backgroundColor = "green";
-						clicked[1].style.backgroundColor = "green";
-
-						clicked[0].disabled = true;
-						clicked[1].disabled = true;
-
-						clicked[0].classList.remove("clicked");
-						clicked[0].classList.remove("clicked");
-					} else {
-						clicked[0].classList.remove("clicked");
-						clicked[0].classList.remove("clicked");
-					}
-				}
-			}
-
-			function isMatch(item1, item2) {
-				for (let i = 0; i < pairs.length; i++) {
-					if (
-						(item1 == pairs[i].word && item2 == pairs[i].match) ||
-						(item2 == pairs[i].word && item1 == pairs[i].match)
-					) {
-						return true;
-					}
-				}
-				return false;
-			}
-		}
-
-		shuffleAssign();
-	});
-} else if (food.value == "index.html") {
-	document.addEventListener("DOMContentLoaded", () => {
-		const pairs = [
+	} else if (food.value == "comidas.html") {
+		pairs = [
 			{ word: "la banana", match: "banana" },
 			{ word: "las frutas", match: "fruits" },
 			{ word: "el limón", match: "lemon" },
@@ -116,83 +42,31 @@ if (food.value == "index.html") {
 			{ word: "las verduras", match: "vegetables" },
 			{ word: "la zanahoria", match: "carrot" }
 		];
-
-		let cards = document.getElementsByClassName("cards");
-		console.log(cards);
-
-		function shuffleArray(array) {
-			for (let i = array.length - 1; i > 0; i--) {
-				// Generate a random index from 0 to i
-				const j = Math.floor(Math.random() * (i + 1));
-				// Swap elements at indices i and j
-				[array[i], array[j]] = [array[j], array[i]];
-			}
-			return array;
-		}
-
-		function shuffleAssign() {
-			// shuffle list of pairs
-			let shuffledPairs = shuffleArray(pairs);
-			console.log(shuffledPairs);
-
-			//create a new array for selected pairs this round
-			let cardPairs = [];
-
-			//add the first cards.length/2 pairs with word and match to new array
-			for (let i = 0; i < cards.length / 2; i++) {
-				cardPairs.push(shuffledPairs[i].word);
-				cardPairs.push(shuffledPairs[i].match);
-			}
-			console.log(cardPairs);
-
-			//shuffle that array
-			cardPairs = shuffleArray(cardPairs);
-
-			// iterate over that array to assign cards
-			for (let i = 0; i < cardPairs.length; i++) {
-				cards[i].innerText = cardPairs[i];
-				cards[i].onclick = isClicked;
-			}
-
-			function isClicked(e) {
-				let cardClicked = e.srcElement;
-				cardClicked.classList.add("clicked");
-				let clicked = document.getElementsByClassName("clicked");
-				if (clicked.length == 2) {
-					if (isMatch(clicked[0].innerText, clicked[1].innerText)) {
-						clicked[0].style.backgroundColor = "green";
-						clicked[1].style.backgroundColor = "green";
-
-						clicked[0].disabled = true;
-						clicked[1].disabled = true;
-
-						clicked[0].classList.remove("clicked");
-						clicked[0].classList.remove("clicked");
-					} else {
-						clicked[0].classList.remove("clicked");
-						clicked[0].classList.remove("clicked");
-					}
-				}
-			}
-
-			function isMatch(item1, item2) {
-				for (let i = 0; i < pairs.length; i++) {
-					if (
-						(item1 == pairs[i].word && item2 == pairs[i].match) ||
-						(item2 == pairs[i].word && item1 == pairs[i].match)
-					) {
-						return true;
-					}
-				}
-				return false;
-			}
-		}
-
-		shuffleAssign();
-	});
-} else if (food.value == "index.html") {
-	document.addEventListener("DOMContentLoaded", () => {
-		const pairs = [
+	} else if (food.value == "frutas.html") {
+		pairs = [
+			{ word: "la banana", match: "banana" },
+			{ word: "las frutas", match: "fruits" },
+			{ word: "el limón", match: "lemon" },
+			{ word: "la manzana", match: "apple" },
+			{ word: "el melocotón", match: "peach" },
+			{ word: "la naranja", match: "orange" },
+			{ word: "los pera", match: "pear" },
+			{ word: "la uva", match: "grapes" },
+			{ word: "las arvejas", match: "peas" },
+			{ word: "la cebolla", match: "onion" },
+			{ word: "el champiñión", match: "mushroom" },
+			{ word: "la ensalada", match: "salad" },
+			{ word: "los espárragos", match: "asparagus" },
+			{ word: "los frijoles", match: "beans" },
+			{ word: "la lechuga", match: "lettuce" },
+			{ word: "el maíz", match: "corn" },
+			{ word: "las papas/patatas fritas", match: "french fries" },
+			{ word: "el tomate", match: "tomato" },
+			{ word: "las verduras", match: "vegetables" },
+			{ word: "la zanahoria", match: "carrot" }
+		];
+	} else if (food.value == "carne.html") {
+		pairs = [
 			{ word: "el atón", match: "tuna" },
 			{ word: "el bistec", match: "steak" },
 			{ word: "los camarones", match: "shrimp" },
@@ -209,83 +83,8 @@ if (food.value == "index.html") {
 			{ word: "la salchicha", match: "sausage" },
 			{ word: "el salmón", match: "salmon" }
 		];
-
-		let cards = document.getElementsByClassName("cards");
-		console.log(cards);
-
-		function shuffleArray(array) {
-			for (let i = array.length - 1; i > 0; i--) {
-				// Generate a random index from 0 to i
-				const j = Math.floor(Math.random() * (i + 1));
-				// Swap elements at indices i and j
-				[array[i], array[j]] = [array[j], array[i]];
-			}
-			return array;
-		}
-
-		function shuffleAssign() {
-			// shuffle list of pairs
-			let shuffledPairs = shuffleArray(pairs);
-			console.log(shuffledPairs);
-
-			//create a new array for selected pairs this round
-			let cardPairs = [];
-
-			//add the first cards.length/2 pairs with word and match to new array
-			for (let i = 0; i < cards.length / 2; i++) {
-				cardPairs.push(shuffledPairs[i].word);
-				cardPairs.push(shuffledPairs[i].match);
-			}
-			console.log(cardPairs);
-
-			//shuffle that array
-			cardPairs = shuffleArray(cardPairs);
-
-			// iterate over that array to assign cards
-			for (let i = 0; i < cardPairs.length; i++) {
-				cards[i].innerText = cardPairs[i];
-				cards[i].onclick = isClicked;
-			}
-
-			function isClicked(e) {
-				let cardClicked = e.srcElement;
-				cardClicked.classList.add("clicked");
-				let clicked = document.getElementsByClassName("clicked");
-				if (clicked.length == 2) {
-					if (isMatch(clicked[0].innerText, clicked[1].innerText)) {
-						clicked[0].style.backgroundColor = "green";
-						clicked[1].style.backgroundColor = "green";
-
-						clicked[0].disabled = true;
-						clicked[1].disabled = true;
-
-						clicked[0].classList.remove("clicked");
-						clicked[0].classList.remove("clicked");
-					} else {
-						clicked[0].classList.remove("clicked");
-						clicked[0].classList.remove("clicked");
-					}
-				}
-			}
-
-			function isMatch(item1, item2) {
-				for (let i = 0; i < pairs.length; i++) {
-					if (
-						(item1 == pairs[i].word && item2 == pairs[i].match) ||
-						(item2 == pairs[i].word && item1 == pairs[i].match)
-					) {
-						return true;
-					}
-				}
-				return false;
-			}
-		}
-
-		shuffleAssign();
-	});
-} else if (food.value == "index.html") {
-	document.addEventListener("DOMContentLoaded", () => {
-		const pairs = [
+	} else if (food.value == "otras.html") {
+		pairs = [
 			{ word: "el aceite", match: "oil" },
 			{ word: "el ajo", match: "garlic" },
 			{ word: "el arroz", match: "rice" },
@@ -302,85 +101,10 @@ if (food.value == "index.html") {
 			{ word: "el sándwich", match: "sandwich" },
 			{ word: "la sopa", match: "soup" },
 			{ word: "el vinagre", match: "vinegar" },
-			{ word: "el yogur", match: "yougurt" }
+			{ word: "el yogur", match: "yogurt" }
 		];
-
-		let cards = document.getElementsByClassName("cards");
-		console.log(cards);
-
-		function shuffleArray(array) {
-			for (let i = array.length - 1; i > 0; i--) {
-				// Generate a random index from 0 to i
-				const j = Math.floor(Math.random() * (i + 1));
-				// Swap elements at indices i and j
-				[array[i], array[j]] = [array[j], array[i]];
-			}
-			return array;
-		}
-
-		function shuffleAssign() {
-			// shuffle list of pairs
-			let shuffledPairs = shuffleArray(pairs);
-			console.log(shuffledPairs);
-
-			//create a new array for selected pairs this round
-			let cardPairs = [];
-
-			//add the first cards.length/2 pairs with word and match to new array
-			for (let i = 0; i < cards.length / 2; i++) {
-				cardPairs.push(shuffledPairs[i].word);
-				cardPairs.push(shuffledPairs[i].match);
-			}
-			console.log(cardPairs);
-
-			//shuffle that array
-			cardPairs = shuffleArray(cardPairs);
-
-			// iterate over that array to assign cards
-			for (let i = 0; i < cardPairs.length; i++) {
-				cards[i].innerText = cardPairs[i];
-				cards[i].onclick = isClicked;
-			}
-
-			function isClicked(e) {
-				let cardClicked = e.srcElement;
-				cardClicked.classList.add("clicked");
-				let clicked = document.getElementsByClassName("clicked");
-				if (clicked.length == 2) {
-					if (isMatch(clicked[0].innerText, clicked[1].innerText)) {
-						clicked[0].style.backgroundColor = "green";
-						clicked[1].style.backgroundColor = "green";
-
-						clicked[0].disabled = true;
-						clicked[1].disabled = true;
-
-						clicked[0].classList.remove("clicked");
-						clicked[0].classList.remove("clicked");
-					} else {
-						clicked[0].classList.remove("clicked");
-						clicked[0].classList.remove("clicked");
-					}
-				}
-			}
-
-			function isMatch(item1, item2) {
-				for (let i = 0; i < pairs.length; i++) {
-					if (
-						(item1 == pairs[i].word && item2 == pairs[i].match) ||
-						(item2 == pairs[i].word && item1 == pairs[i].match)
-					) {
-						return true;
-					}
-				}
-				return false;
-			}
-		}
-
-		shuffleAssign();
-	});
-} else {
-	document.addEventListener("DOMContentLoaded", () => {
-		const pairs = [
+	} else if (food.value == "bebidas.html") {
+		pairs = [
 			{ word: "el agua (mineral)", match: "(mineral) water" },
 			{ word: "la bebida", match: "drink" },
 			{ word: "el café", match: "coffee" },
@@ -391,78 +115,105 @@ if (food.value == "index.html") {
 			{ word: "el té (helado)", match: "(iced) tea" },
 			{ word: "el vino (blanco/tinto)", match: "(white/red) wine" }
 		];
+	}
 
-		let cards = document.getElementsByClassName("cards");
-		console.log(cards);
+	let cards = document.getElementsByClassName("cards");
+	console.log(cards);
 
-		function shuffleArray(array) {
-			for (let i = array.length - 1; i > 0; i--) {
-				// Generate a random index from 0 to i
-				const j = Math.floor(Math.random() * (i + 1));
-				// Swap elements at indices i and j
-				[array[i], array[j]] = [array[j], array[i]];
-			}
-			return array;
+	function shuffleArray(array) {
+		for (let i = array.length - 1; i > 0; i--) {
+			// Generate a random index from 0 to i
+			const j = Math.floor(Math.random() * (i + 1));
+			// Swap elements at indices i and j
+			[array[i], array[j]] = [array[j], array[i]];
 		}
+		return array;
+	}
 
-		function shuffleAssign() {
-			// shuffle list of pairs
-			let shuffledPairs = shuffleArray(pairs);
-			console.log(shuffledPairs);
+	function shuffleAssign() {
+		// shuffle list of pairs
+		let shuffledPairs = shuffleArray(pairs);
+		console.log(shuffledPairs);
 
-			//create a new array for selected pairs this round
-			let cardPairs = [];
+		//create a new array for selected pairs this round
+		let cardPairs = [];
 
-			//add the first cards.length/2 pairs with word and match to new array
-			for (let i = 0; i < cards.length / 2; i++) {
-				cardPairs.push(shuffledPairs[i].word);
-				cardPairs.push(shuffledPairs[i].match);
-			}
-			console.log(cardPairs);
+		//add the first cards.length/2 pairs with word and match to new array
+		for (let i = 0; i < cards.length / 2; i++) {
+			cardPairs.push({ word: shuffledPairs[i].word, type: "word" });
+			cardPairs.push({ word: shuffledPairs[i].match, type: "image" });
+		}
+		console.log(cardPairs);
 
-			//shuffle that array
-			cardPairs = shuffleArray(cardPairs);
+		//shuffle that array
+		cardPairs = shuffleArray(cardPairs);
 
-			// iterate over that array to assign cards
-			for (let i = 0; i < cardPairs.length; i++) {
-				cards[i].innerText = cardPairs[i];
+		// iterate over that array to assign cards
+		for (let i = 0; i < cardPairs.length; i++) {
+			if (cardPairs[i].type == "word") {
+				cards[i].innerHTML = "<p>" + cardPairs[i].word + "</p>";
 				cards[i].onclick = isClicked;
 			}
-
-			function isClicked(e) {
-				let cardClicked = e.srcElement;
-				cardClicked.classList.add("clicked");
-				let clicked = document.getElementsByClassName("clicked");
-				if (clicked.length == 2) {
-					if (isMatch(clicked[0].innerText, clicked[1].innerText)) {
-						clicked[0].style.backgroundColor = "green";
-						clicked[1].style.backgroundColor = "green";
-
-						clicked[0].disabled = true;
-						clicked[1].disabled = true;
-
-						clicked[0].classList.remove("clicked");
-						clicked[0].classList.remove("clicked");
-					} else {
-						clicked[0].classList.remove("clicked");
-						clicked[0].classList.remove("clicked");
-					}
-				}
-			}
-
-			function isMatch(item1, item2) {
-				for (let i = 0; i < pairs.length; i++) {
-					if (
-						(item1 == pairs[i].word && item2 == pairs[i].match) ||
-						(item2 == pairs[i].word && item1 == pairs[i].match)
-					) {
-						return true;
-					}
-				}
-				return false;
+			if (cardPairs[i].type == "image") {
+				cards[i].innerHTML = '<img class="words" src="images/' + cardPairs[i].word + '">';
+				cards[i].onclick = isClicked;
 			}
 		}
 
+		function isClicked(e) {
+			let cardClicked = e.srcElement;
+			if (cardClicked.localName == "p" || cardClicked.localName == "div") {
+				console.log("word");
+				console.log(cardClicked.innerText);
+			} else {
+				console.log("image");
+				//console.log(cardClicked.src);
+				//let text = cardClicked.src;
+				//const myArray = text.split("/");
+				//let imagename = myArray[5];
+				//console.log(imagename);
+			}
+
+			console.log(cardClicked.children.length);
+			cardClicked.classList.add("clicked");
+			let clicked = document.getElementsByClassName("clicked");
+			if (clicked.length == 2) {
+				if (isMatch(clicked[0].innerText, clicked[1].innerText)) {
+					clicked[0].style.backgroundColor = "green";
+					clicked[1].style.backgroundColor = "green";
+
+					clicked[0].disabled = true;
+					clicked[1].disabled = true;
+
+					clicked[0].classList.remove("clicked");
+					clicked[0].classList.remove("clicked");
+				} else {
+					clicked[0].classList.remove("clicked");
+					clicked[0].classList.remove("clicked");
+				}
+			}
+		}
+		function isMatch(item1, item2) {
+			for (let i = 0; i < pairs.length; i++) {
+				if (
+					(item1 == pairs[i].word && item2 == pairs[i].match) ||
+					(item2 == pairs[i].word && item1 == pairs[i].match)
+				) {
+					let score = 0;
+					score = score + 1;
+					document.getElementById("score").innerText = score;
+					return true;
+				}
+			}
+			return false;
+		}
+	}
+
+	function restart() {
+		score = 0;
+		shuffleArray();
 		shuffleAssign();
-	});
-}
+	}
+
+	shuffleAssign();
+});
